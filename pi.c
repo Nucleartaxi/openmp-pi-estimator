7a73c46fd2ee08198c4b8c09e295ee8e7a2f877f
Alex Shirk <alex@alexshirk.com>
2023-10-24 23:06:37 -0700

    compute hits function

--- pi.c
+++ pi.c
@@ -13,9 +13,7 @@
 
 int p=1;
 
-int *generateArray(int);
-void dispArray(int *,int);
-void computeSum(int *,int );
+void computeHits(int );
 
 int main(int argc, char *argv[])
 {
@@ -51,16 +49,10 @@ int main(int argc, char *argv[])
 
 	double time = omp_get_wtime();
 
-	// 1. generate the array
-	//
-	//
-	int *a=NULL;
-	a = generateArray(n);
-
 	//dispArray(a,n);
 
 	// 2. compute sum using reduce
-	computeSum(a,n);
+	computeHits(n);
 
 
 	
@@ -70,76 +62,38 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
-/* This function generates an array of size n in parallel (across p threads).
- * */
-int *generateArray(int n) {
-
-	int bound = 20;
-
-	int *a = (int *)malloc(sizeof(int)*n);
-	assert(a!=NULL);
-
-	printf("n=%d, p=%d\n",n,p);
-
-	omp_set_num_threads(p);
-
-	//#pragma omp parallel default(none) shared(a,n) private (i)
-	#pragma omp parallel
-	{
-
-		int i=0;
-		#pragma omp for 
-		for(i = 0; i < n; i++)  //Can you think of why we need this to be #pragma omp for? (as opposed to just a for loop)?
-		{
-
-			int rank = omp_get_thread_num();
-			int seed = rank+1;
-			seed = seed*i;		// I am trying to make "seed" a function of the thread rank and the iteration variable 
-			
-			//printf("rank=%d: seed = %d, i=%d,n=%d\n",rank,seed,i,n);
-			a[i] = rand_r(&seed) % bound; //rand_r makes it the reentrant version
-
-		}
-
-	}
-
-	return a;
-} // end generateArray
-
 
 /* This function computes the sum of the n elements in array a, in parallel (across p threads).
  */
-void computeSum(int *a,int n) {
+int inCircle(double x, double y) {
+	//(x-a)^2 + (y-b)^2 = r^2, a, b, and r are .5
+	return (x - 0.5) * (x - 0.5) + (y - 0.5) * (y - 0.5) < 0.5 * 0.5;
+}
+double randomDouble() { //generates a random double between 0 and 1.
+	return rand() / (double) RAND_MAX;
+}
+void computeHits(int n) {
 
 	omp_set_num_threads(p);
 
 	int i;
-	int sum=0;
+	int hits=0;
 
-	#pragma omp parallel for shared(a,n) reduction(+:sum) 
+	#pragma omp parallel for shared(n) reduction(+:hits) 
 	for(i=0;i<n;i++) {
-		//printf("i=%d, n=%d, sum=%d,a[i]=%d\n",i,n,sum,a[i]);
+		double x = randomDouble();
+		double y = randomDouble();
+		if (inCircle(x, y)) {
+			hits++;
+		}
+		//printf("i=%d, n=%d, hits=%d,a[i]=%d\n",i,n,hits,a[i]);
 		//fflush(stdout);
-		sum+=a[i];	// Q) will this be a thread-safe write?
 	} // end for
 
-	// Q) The sum variable is being written onto 
+	// Q) The hits variable is being written onto 
 
-	// Q) Which thread will have the final sum to print?
-	printf("sum=%d\n",sum);
+	// Q) Which thread will have the final hits to print?
+	printf("hits=%d\n",hits);
 
-}// end computeSum
-
-/* This is a simple display function for the array for your testing purposes.
- */
-
-void dispArray(int *a,int n) {
-	int i;
-	printf("********************\n");
-	printf("Debug: Array a is: \n");
-	for(i=0;i<n;i++) {
-		printf("%d\n",a[i]);
-	}
-	printf("********************\n");
-}
+}// end computeHits
 
